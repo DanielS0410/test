284b400fddd83661b495b099206b3a54ad29cdf4
DanielS0410 <119893724+DanielS0410@users.noreply.github.com>
2022-12-06 12:43:16 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-st.write('Hello World')
+
+st.title('My First App')
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
