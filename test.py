4bab1ee58e41ac5bfd080dcd02a94d40b2fcb95b
DanielS0410 <119893724+DanielS0410@users.noreply.github.com>
2022-12-06 13:32:50 +0100

    Update test.py

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ df = pd.read_csv('Bastar Craton.csv')
 
 sel2 = st.selectbox('1. Selection', ['Mg','Si','Cr'])
 #sel3 = st.selectbox('2. Selection', ['Mg','Si','Cr'])
-pr = range(46)
+pr = [0:45]
 x = np.linspace(0, 50, 50)
 fig, ax = plt.subplots(46)
 
