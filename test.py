5e17a8f231c7730318dce7de609601157a16c78c
DanielS0410 <119893724+DanielS0410@users.noreply.github.com>
2022-12-06 12:53:11 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,9 @@ import numpy as np
 
 st.title('My First App')
 
-st.selectbox('1', '2', '3')
+sel1 = st.selectbox('Selection', [1,2,3])
+
+st.write('Deine Auswahl ist:', sel1)
 
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
